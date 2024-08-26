cbe4c61a23cd6d6dfbeb7c09a1a1491715aa0da9
mylesz123 <mylesz123@gmail.com>
2024-08-25 21:33:49 -0400

    configure type aliasing, and TS module augmentation

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
@@ -11,6 +12,16 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    modules: {
+      scopeBehaviour: 'local'
     }
   }
 })
